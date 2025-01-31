1050ba73509e23f54225d295e6604e6510f6eac3
Devid Duma <devidd05@gmail.com>
2025-01-31 20:08:45 +0100

    feat(TaskViewComponent): Add createNewList method to New List button

--- frontend/src/app/pages/task-view/task-view.component.ts
+++ frontend/src/app/pages/task-view/task-view.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {TaskService} from "../../services/task.service";
 
 @Component({
   selector: 'app-task-view',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskViewComponent implements OnInit {
 
-  constructor() { }
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
   }
 
+  createNewList(title: string) {
+    this.taskService.createList(title).subscribe((response: any) => {
+      console.log(response);
+    });
+  }
 }
