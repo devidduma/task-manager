f15bc85b79673a299248cf48deafe9e3984c3d14
Devid Duma <devidd05@gmail.com>
2025-02-02 18:52:03 +0100

    feat(AppJS): CORS Header Access-Control-Allow-Methods to allow all methods

--- api/app.js
+++ api/app.js
@@ -13,6 +13,7 @@ app.use(bodyParser.json());
 // https://enable-cors.org/server_expressjs.html
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods", "GET,POST,HEAD,OPTIONS,PUT,PATCH,DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
