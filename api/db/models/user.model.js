01d3ca637882d456a601fc8459fa157c1b85e32d
Devid Duma <devidd05@gmail.com>
2025-02-05 14:51:31 +0100

    fix(UserModelJS): Fix bug on find user by id and token

--- api/db/models/user.model.js
+++ api/db/models/user.model.js
@@ -97,7 +97,7 @@ UserSchema.statics.findByIdAndToken = function(_id, token) {
 
     return User.findOne({
         _id,
-        'session.token': token
+        'sessions.token': token
     });
 }
 
