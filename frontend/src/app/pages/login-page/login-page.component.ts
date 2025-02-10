a8a6a075ab68f13b60c9471456d00de8a2afe8b9
Devid Duma <devidd05@gmail.com>
2025-02-10 22:29:36 +0100

    feat(LoginPageComponent): After successfully logging in, redirect to Lists page

--- frontend/src/app/pages/login-page/login-page.component.ts
+++ frontend/src/app/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../../services/auth.service";
 import {HttpResponse} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-login-page',
@@ -10,7 +11,7 @@ import {HttpResponse} from "@angular/common/http";
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   ngOnInit(): void {
@@ -18,6 +19,10 @@ export class LoginPageComponent implements OnInit {
 
   onLoginButtonClicked(email: string, password: string): void {
     this.authService.login(email, password).subscribe((res: HttpResponse<any>) => {
+      if(res.status === 200) {
+        // we have logged in successfully
+        this.router.navigate(['/lists']);
+      }
       console.log(res);
     });
   }
