de0e42a422a170cc3b32df87277ef2ab39da2331
Devid Duma <devidd05@gmail.com>
2025-02-11 20:45:02 +0100

    feat(SignupPageComponent): After successfully signing up, redirect to Lists page

--- frontend/src/app/pages/signup-page/signup-page.component.ts
+++ frontend/src/app/pages/signup-page/signup-page.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpResponse} from "@angular/common/http";
 import {AuthService} from "../../services/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-signup-page',
@@ -10,13 +11,17 @@ import {AuthService} from "../../services/auth.service";
 })
 export class SignupPageComponent implements OnInit {
 
-  constructor(private authService: AuthService) {
+  constructor(private authService: AuthService, private router: Router) {
   }
 
   ngOnInit() {}
 
   onSignupButtonClicked(email: string, password: string): void {
     this.authService.signup(email, password).subscribe((res: HttpResponse<any>) => {
+      if(res.status === 200) {
+        // we have logged in successfully
+        this.router.navigate(['/lists']);
+      }
       console.log(res);
     });
   }
